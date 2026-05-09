b103467451f4ebc7ab60db148c923bf38bc93a46
yasindumalmith <yasindumalmith058@gmail.com>
2026-05-10 02:05:20 +0530

    test workflow-1

--- src/main/java/com/example/devflowsample/controller/TestController.java
+++ src/main/java/com/example/devflowsample/controller/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String test() {
-        return "test";
+        return "test -1";
     }
 }
